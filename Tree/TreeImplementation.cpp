4d48680b1d08ea7dac759ca36b1566fafe9666ad
kartikey <kartikkey0414@gmail.com>
2025-04-16 10:48:09 +0530

    Added basic tree template

---
+++ Tree/TreeImplementation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int data;
+    struct TreeNode* left;
+    struct TreeNode* right;
+
+    TreeNode(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+int main(){
+
+    TreeNode* root = new TreeNode(10);
+    root->left = new TreeNode(20);
+    root->right = new TreeNode(30);
+    root->left->left = new TreeNode(40);
+    root->left->right = new TreeNode(50);
+    root->right->left = new TreeNode(60);
+    root->right->right = new TreeNode(70);
+
+}
